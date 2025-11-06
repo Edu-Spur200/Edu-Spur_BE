96bad9b5cf0e8617f199668d898ce461fd5d11a5
Somtochukwu Emmanuel <somtochukwue98@gmail.com>
2025-11-07 00:06:38 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ['http://localhost:5173' , 'https://edu-spur.netlify.app/'],
+  origin: ['http://localhost:5173' , 'https://edu-spur.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type'  , 'Authorization'],
   credentials: true,
